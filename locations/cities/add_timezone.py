c2cae5644863d70e6cc722c952f376581e6af0e4
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2017-07-24 17:15:19 +0430

    added timezone field for cities

--- locations/cities/add_timezone.py
+++ locations/cities/add_timezone.py
@@ -1,34 +1,51 @@
 import json
-
-__author__ = 'Rfun'
-
 import traceback
 from tqdm import tqdm
 from pymongo import MongoClient
+import requests
+
+__author__ = 'Rfun'
 
 client = MongoClient()
 db = client.joojoo
-import requests
 
 with open('./data/cities.json') as f:
     all_cities = json.load(f)
 
-def main():
+
+def get_lat_lng_of_cities():
+    available_usernames = ['mega', 'sarah', 'barak', 'smith', 'Rimon']
+    iterator = 0
     cities = []
+    max_number_of_tries = 3
+    number_of_sequential_tries = 0
     for city in tqdm(all_cities):
         try:
             lat = city['lat']
             lng = city['lng']
-            url = "http://api.geonames.org/timezoneJSON?formatted=true&lat=" + lat + "&" + "lng=" + lng + "&username=Rimon&style=full"
+            url = "http://api.geonames.org/timezoneJSON?formatted=true&lat=" + lat + "&" + \
+                  "lng=" + lng + "&" + "username=" + available_usernames[iterator] +"&style=full"
             result = requests.get(url).json()
-            city['timezon_id'] = result['timezone_id']
-            db.cities.update({"_id": city["_id"]},
-                             {"$set": {"country_code": result['countryCode'], 'timezone_id': result['timezoneId'],
-                                       'country_english_name': result['countryName']}})
-        except:
+            city['timezone_id'] = result['timezoneId']
+            cities.append(city)
+            number_of_sequential_tries = 0
+        except IOError:
             traceback.print_exc()
             print(city['english_name'])
+        except KeyError:
+            number_of_sequential_tries += 1
+            if number_of_sequential_tries == max_number_of_tries:
+                number_of_sequential_tries = 0
+                print("time limit exceeded in  " + str(all_cities.index(city)) + " , we change the username :D")
+                iterator += 1
 
+    return cities
+
+
+def main():
+    cities = get_lat_lng_of_cities()
+    with open('data/cities.json', 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(cities, ensure_ascii=False))
 
 
 if __name__ == '__main__':
--- locations/province/main.py
+++ locations/province/main.py
@@ -1,2 +1,71 @@
 __author__ = 'Rfun'
 
+import csv
+import difflib
+
+
+from tqdm import tqdm
+from translator import translate
+import json
+
+'''
+Here I supposed that you've run the countries/main
+and now there is a countries.json file in the data
+subdirectory of countries directory
+'''
+
+with open('./../countries/data/countries.json') as f:
+    all_countries = json.load(f)
+    all_countries_standard_name = []
+    for item in all_countries:
+        all_countries_standard_name.append(item['english_name'])
+
+
+def load_provinces_with_their_country():
+    print("=============Loading provinces=============")
+    provinces = []
+    with open('./../source_files/world_cities.csv') as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            english_name = row['province']
+
+            try:
+                country = difflib.get_close_matches(row['country'], all_countries_standard_name)[0]
+            except:
+                country = row['country']
+                print("couldnt find the country for city" + english_name + " with the country " + country)
+
+            province = {'english_name': english_name , 'country' : country}
+            provinces.append(province)
+
+    print("=============Loaded=============")
+    return provinces
+
+
+def add_persian_name(provinces):
+    print("=============Adding persian names=============")
+    batch_size = 10
+
+    for iter in tqdm(range(0, len(provinces), batch_size)):
+        english_names = [province['english_name'] for province in provinces[iter: iter + batch_size]]
+        persian_names = translate.batch_translate(english_names, "en", "fa")
+        if persian_names is not None:
+            for i in range(len(english_names)):
+                provinces[iter + i]['persian_name'] = persian_names[i]
+        else:
+            for i in range(len(english_names)):
+                provinces[iter + i]['persian_name'] = ""
+
+    return provinces
+
+
+def main():
+    provinces = load_provinces_with_their_country()
+    provinces = add_persian_name(provinces)
+    with open('data/provinces.json', 'w+', encoding='utf-8') as f:
+        f.write(json.dumps(provinces, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
